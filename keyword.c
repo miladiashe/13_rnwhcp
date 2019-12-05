c4b1416101f2d3189531e2a54dac6c82f2b62838
miladiashe <55428028+miladiashe@users.noreply.github.com>
2019-12-05 15:45:15 +0900

    14주 실습1

--- keyword.c
+++ keyword.c
@@ -3,26 +3,29 @@
 #include <stdlib.h>
 #include <string.h> 
 
+#define NKEYS 7
+
 struct keytat{
 	int nums; //나온횟수 
 	char name[MAX_NAME];
 };
 
 
-struct keytat keywords[6] = {
+struct keytat keywords[NKEYS] = {
 	{0, "int"},
 	{0, "float"},
 	{0, "return"},
 	{0, "if"},
 	{0, "for"},
-	{0, "while"}
+	{0, "while"},
+	{0, "scanf"}
 };
 
 void count_word(char* word)
 {
 	//각 keyword 별로
 	int i;
-	for (i=0; i<6; i++)
+	for (i=0; i<NKEYS; i++)
 	{
 		//wordf랑 keywrd랑 같은문자 나오면 
 		if(strncmp(word, keywords[i].name, strlen(keywords[i].name))==0)
@@ -36,7 +39,7 @@ void count_word(char* word)
 void print_word()
 {
 	int i;
-	for(i=0; i<6; i++)
+	for(i=0; i<NKEYS; i++)
 	{
 		printf("%s : %i\n", keywords[i].name, keywords[i].nums);
 	}
